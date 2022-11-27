d461e4776ead019459c30ee24b925310a756c216
Shahzer Baig <shahzer.baig.sb@gmail.com>
2022-11-27 16:20:26 +0530

    Crypto Tracker Link Added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import '../styles/card.css'
+import { useNavigate } from 'react-router';
 export const Card = (props) => {
+    const navigate = useNavigate();
     let title = props.title;
     let description = props.description
     return (
         <div className='card'>
-            <h3>{title}</h3>
-            <p>{description}</p>
+            <a href={props.link} target="_blank">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </a>
+            
+            
         </div>
     )
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,7 +3,11 @@ import { Card } from './Card';
 import '../styles/slider.css';
 
 const project = [
-    {title:'Crypto Tracker',desc:' A cryptocurrency price tracker monitors the current value of different digital currencies and tokens. Many of these services provide historical data as well, allowing users to compare current prices against older values.'},
+    {
+        title:'Crypto Tracker',
+        desc:' A cryptocurrency price tracker monitors the current value of different digital currencies and tokens. Many of these services provide historical data as well, allowing users to compare current prices against older values.',
+        link:"https://shahzerbaig.github.io/CryptoTracker/"
+    },
     {title:'Dashboard',desc:'A data dashboard is a tool many businesses use to track, analyze, and display data—usually to gain insight into the overall wellbeing of an organization, department, or specific process.'},
     {title:'Url Shortner',desc:'ShortURL is a url shortener to reduce a long link. Use our tool to shorten links and then share them, in addition you can monitor traffic statistics'},
     {title:'Chat Application',desc:'A chat application makes it easy to communicate with people anywhere in the world by sending and receiving messages in real time.'}
@@ -11,7 +15,7 @@ const project = [
 export const Slider = () => {
     return (
         <div id='slider'>
-            <Card title={project[0].title} description={project[0].desc} />
+            <Card title={project[0].title} description={project[0].desc} link={project[0].link} />
             <Card title={project[1].title} description={project[1].desc} />
             <Card title={project[2].title} description={project[2].desc} />
             <Card title={project[3].title} description={project[3].desc} />
